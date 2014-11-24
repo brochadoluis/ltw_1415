60733b0ffd9ed47ef81bf48e2c24c8bd03e291c2
Luis Reis <brochadoluis@gmail.com>
2014-11-24 10:04:03 +0000

    DB re-arranged

--- devel/database/db.sql
+++ devel/database/db.sql
@@ -10,16 +10,17 @@ DROP TABLE IF EXISTS Message CASCADE;
 
 
 CREATE TYPE permission AS ENUM ('public', 'private');
+CREATE TYPE state AS ENUM ('opened', 'closed');
 
 
 /* Users */
 CREATE TABLE IF NOT EXISTS User (
-	idUser		SERIAL PRIMARY KEY,
-  username VARCHAR(16) NOT NULL,
-	name 		VARCHAR 		NOT NULL,
+  idUser   SERIAL PRIMARY KEY,
+  username VARCHAR(16)     NOT NULL,
+  name     VARCHAR         NOT NULL,
 	localidade 	VARCHAR 		NOT NULL,
-	passwor 	VARCHAR 		NOT NULL,
-	email 		VARCHAR UNIQUE 	NOT NULL,
+  passwor  VARCHAR         NOT NULL,
+  email    VARCHAR UNIQUE  NOT NULL,
 	ocupation 	VARCHAR 		NOT NULL,
 	fotograph 	VARCHAR,
 	CHECK (char_length(pass) > 8),
@@ -28,27 +29,30 @@ CREATE TABLE IF NOT EXISTS User (
 
 /* Polls */
 CREATE TABLE IF NOT EXISTS Poll (
-	idPoll 		SERIAL PRIMARY KEY,
-	title 		VARCHAR(85) 	NOT NULL,
-	data_post 	DATE DEFAULT CURRENT_DATE,
-	fotograph 	VARCHAR 		NOT NULL,
-	idUser 		INTEGER REFERENCES User 	ON DELETE CASCADE
-	idQuestion 	INTEGER REFERENCES Question ON DELETE CASCADE
-	idAnswer 	INTEGER REFERENCES Answer 	ON DELETE CASCADE
-	permissionPoll permission;
+  idPoll         SERIAL PRIMARY KEY,
+  title          VARCHAR(85) NOT NULL,
+  data_post      DATE DEFAULT CURRENT_DATE,
+  fotograph      VARCHAR     NOT NULL,
+  idCreator      INTEGER REFERENCES User ON DELETE CASCADE,
+  idQuestion     INTEGER REFERENCES Question ON DELETE CASCADE,
+  idAnswer       INTEGER REFERENCES Answer ON DELETE CASCADE,
+  pollPermission permission,
+  pollState      state
+
 );
 
 /* Questions */
 CREATE TABLE IF NOT EXISTS Question (
 	idQuestion 	SERIAL PRIMARY KEY,
-	idPoll 		INTEGER REFERENCES Poll 	ON DELETE CASCADE
+  idPoll INTEGER REFERENCES Poll ON DELETE CASCADE,
 	idAnswer 	INTEGER REFERENCES Answer 	ON DELETE CASCADE
 );
 
 /* Answers */
 CREATE TABLE IF NOT EXISTS Answer (
-	idAnswer 	SERIAL PRIMARY KEY,
-	idQuestion 	INTEGER REFERENCES Question ON DELETE CASCADE
+  idAnswer     SERIAL PRIMARY KEY,
+  idRespondent INTEGER REFERENCES User ON DELETE CASCADE,
+  idQuestion   INTEGER REFERENCES Question ON DELETE CASCADE
 );
 
 /* Links */
@@ -60,22 +64,22 @@ CREATE TABLE IF NOT EXISTS Link (
 /* Categories */
 CREATE TABLE IF NOT EXISTS Category (
 	idCategory	SERIAL 	PRIMARY KEY,
-	name 		VARCHAR NOT NULL
+  name VARCHAR NOT NULL
 );
 
 /* Polls Categories */
 CREATE TABLE IF NOT EXISTS PollCategory (
-  idPoll 	INTEGER,
+  idPoll INTEGER,
   name      VARCHAR,
-  CONSTRAINT UNIQUE (idPoll, name), 
-  FOREIGN KEY (idPoll) 	REFERENCES Poll 		ON DELETE CASCADE,
-  FOREIGN KEY (name) 	REFERENCES Category 	ON DELETE CASCADE
+  UNIQUE (idPoll, name),
+  FOREIGN KEY (idPoll) REFERENCES Poll ON DELETE CASCADE,
+  FOREIGN KEY (NAME) REFERENCES CATEGORY ON DELETE CASCADE
 );
 /* Messages */
 CREATE TABLE IF NOT EXISTS Message (
   idMessage 	SERIAL PRIMARY KEY,
-  sender    	VARCHAR(16) REFERENCES User ON DELETE CASCADE,
-  receiver   	VARCHAR(16) REFERENCES User ON DELETE CASCADE,
-  title     	VARCHAR      NOT NULL,
-  content   	VARCHAR(140) NOT NULL
+  sender   VARCHAR(16) REFERENCES User ON DELETE CASCADE,
+  receiver VARCHAR(16) REFERENCES User ON DELETE CASCADE,
+  title    VARCHAR      NOT NULL,
+  content  VARCHAR(140) NOT NULL
 );
