c10c71addda2c8f6eda3539ec0c8ef553a1f5be7
JoaoSoaresCorreia <joao.93.correia@gmail.com>
2014-12-04 16:47:02 +0000

    Update User.html and button.js

--- devel/javascript/addButton.js
+++ devel/javascript/addButton.js
@@ -1,8 +1,8 @@
 var counter = 2;
-function addInput()
-{ 
+function addInput(){ 
   var newdiv = document.createElement('div');
-  newdiv.innerHTML+='<div id=ans><b>Answer number '+ (counter + 1) +':</b><input id=Answer'+(counter+1)+' name=Answer'+(counter+1)+' type=text maxlength=60 style= width:150px; border:1px solid #999999; /></div>';
+  newdiv.innerHTML+='<b>Answer number '+ (counter + 1) +': </b><input id=answer'+(counter+1)+' name=Answer'+(counter+1)+' class=answer_type'+' type=text maxlength=60 style= width:150px; border:1px solid #999999; />';
+    newdiv.setAttribute("id", "ans");
   document.getElementById('ans').appendChild(newdiv);
   counter++;
 }
